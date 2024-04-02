2f65dd654ce68d82476bb1ff6014a81d14f9fe7e
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-04-02 11:10:23 +0200

    Init

--- README.md
+++ README.md
@@ -1,28 +1,39 @@
 <a name="readme-top"></a>
 
-![GitHub tag (with filter)](https://img.shields.io/github/v/tag/KitsuneLab-Development/CS2_TemplateRepo?style=for-the-badge&label=Version)
-![GitHub Repo stars](https://img.shields.io/github/stars/KitsuneLab-Development/CS2_TemplateRepo?style=for-the-badge)
-![GitHub issues](https://img.shields.io/github/issues/KitsuneLab-Development/CS2_TemplateRepo?style=for-the-badge)
-![GitHub](https://img.shields.io/github/license/KitsuneLab-Development/CS2_TemplateRepo?style=for-the-badge)
-![GitHub all releases](https://img.shields.io/github/downloads/KitsuneLab-Development/CS2_TemplateRepo/total?style=for-the-badge)
-![GitHub last commit (branch)](https://img.shields.io/github/last-commit/KitsuneLab-Development/CS2_TemplateRepo/dev?style=for-the-badge)
+![GitHub tag (with filter)](https://img.shields.io/github/v/tag/K4ryuu/CS2-AntiCountry?style=for-the-badge&label=Version)
+![GitHub Repo stars](https://img.shields.io/github/stars/K4ryuu/CS2-AntiCountry?style=for-the-badge)
+![GitHub issues](https://img.shields.io/github/issues/K4ryuu/CS2-AntiCountry?style=for-the-badge)
+![GitHub](https://img.shields.io/github/license/K4ryuu/CS2-AntiCountry?style=for-the-badge)
+![GitHub all releases](https://img.shields.io/github/downloads/K4ryuu/CS2-AntiCountry/total?style=for-the-badge)
+![GitHub last commit (branch)](https://img.shields.io/github/last-commit/K4ryuu/CS2-AntiCountry/dev?style=for-the-badge)
 
 <!-- PROJECT LOGO -->
 <br />
 <div align="center">
   <h1 align="center">KitsuneLab©</h1>
-  <h3 align="center">PLACE OF TITLE</h3>
-  <a align="center">PLACE OF DESCRIPTION</a>
+  <h3 align="center">CS2 Anti-Country</h3>
+  <a align="center">Block players from specific countries from joining the server with whitelist.</a>
 
   <p align="center">
     <br />
-    <a href="https://github.com/KitsuneLab-Development/CS2_TemplateRepo/releases">Download</a>
+    <a href="https://github.com/K4ryuu/CS2-AntiCountry/releases">Download</a>
     ·
-    <a href="https://github.com/KitsuneLab-Development/CS2_TemplateRepo/issues/new?assignees=KitsuneLab-Development&labels=bug&projects=&template=bug_report.md&title=%5BBUG%5D">Report Bug</a>
+    <a href="https://github.com/K4ryuu/CS2-AntiCountry/issues/new?assignees=KitsuneLab-Development&labels=bug&projects=&template=bug_report.md&title=%5BBUG%5D">Report Bug</a>
     ·
-    <a href="https://github.com/KitsuneLab-Development/CS2_TemplateRepo/issues/new?assignees=KitsuneLab-Development&labels=enhancement&projects=&template=feature_request.md&title=%5BREQ%5D">Request Feature</a>
+    <a href="https://github.com/K4ryuu/CS2-AntiCountry/issues/new?assignees=KitsuneLab-Development&labels=enhancement&projects=&template=feature_request.md&title=%5BREQ%5D">Request Feature</a>
   </p>
 </div>
+
+### Support My Work
+
+I offer various ways to support my journey:
+
+- 💬 **Request Private Paid Jobs:** Got a specific project in mind? Let's chat, and I'll provide a quote.
+- 🎁 **Subscribe to My Tiers:** Join one of my subscription tiers for exclusive benefits, early access to projects, and personalized support.
+- ☕ **Buy Me a Coffee:** One-time donations keep me motivated and my creativity flowing.
+- 💼 **Shop from My Paid Resources:** Explore and purchase resources I've crafted for private use.
+
+Your support keeps my creative engine running and allows me to share knowledge with the community. Thanks for being part of my journey.
 
 <!-- ABOUT THE PROJECT -->
 
---
+++ src/CS2AntiCountry.cs
@@ -0,0 +1,108 @@
+﻿using System.Text.Json.Serialization;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using MaxMind.GeoIP2;
+using MaxMind.GeoIP2.Exceptions;
+using static CounterStrikeSharp.API.Core.Listeners;
+using CounterStrikeSharp.API;
+
+namespace CS2CountryBlocker;
+
+public sealed class PluginConfig : BasePluginConfig
+{
+	[JsonPropertyName("Countries")]
+	public List<string> BlockedCountries { get; set; } = new List<string> { "DE", "FR", "HU" };
+
+	[JsonPropertyName("SteamIDWhitelist")]
+	public List<string> SteamIDWhitelist { get; set; } = new List<string> { "76561198345583467" };
+
+	[JsonPropertyName("KickUnknwon")]
+	public bool KickUnknownCountry { get; set; } = true;
+
+	[JsonPropertyName("ConfigVersion")]
+	public override int Version { get; set; } = 1;
+}
+
+[MinimumApiVersion(198)]
+public class PluginCountryBlocker : BasePlugin, IPluginConfig<PluginConfig>
+{
+	public override string ModuleName => "CS2 Country Blocker";
+	public override string ModuleAuthor => "K4ryuu";
+	public override string ModuleDescription => "Block players from specific countries from joining the server.";
+	public override string ModuleVersion => "1.0.0";
+
+	public required PluginConfig Config { get; set; } = new PluginConfig();
+
+	public void OnConfigParsed(PluginConfig config)
+	{
+		if (config.Version < Config.Version)
+		{
+			base.Logger.LogWarning("Configuration version mismatch (Expected: {0} | Current: {1})", this.Config.Version, config.Version);
+		}
+
+		this.Config = config;
+	}
+
+	public override void Load(bool hotReload)
+	{
+		if (!File.Exists(Path.Combine(ModuleDirectory, "GeoLite2-Country.mmdb")))
+		{
+			Logger.LogError("GeoLite2-Country.mmdb not found in the plugin directory. You can download it from https://github.com/P3TERX/GeoLite.mmdb/releases");
+			return;
+		}
+
+		RegisterListener<OnClientConnected>((slot) =>
+		{
+			CCSPlayerController? player = Utilities.GetPlayerFromSlot(slot);
+
+			if (player is null || !player.IsValid || !player.PlayerPawn.IsValid || player.IsBot || player.IsHLTV || player.IpAddress == null)
+				return;
+
+			string countryCode = GetPlayerCountryCode(player);
+
+			if (Config.BlockedCountries.Contains(countryCode, StringComparer.OrdinalIgnoreCase) || (Config.KickUnknownCountry && countryCode == "??"))
+			{
+				if (Config.SteamIDWhitelist.Contains(player.SteamID.ToString()))
+					return;
+
+				Logger.LogInformation($"Blocked player {player.PlayerName} ({player.SteamID}) from joining the server. Country: {countryCode}");
+
+				Server.NextFrame(() =>
+				{
+					Server.ExecuteCommand($"kickid {player.UserId} \"You are not allowed to join this server from this country.\"");
+				});
+			}
+		});
+	}
+
+	public string GetPlayerCountryCode(CCSPlayerController player)
+	{
+		string? playerIp = player.IpAddress;
+
+		if (playerIp == null)
+			return "??";
+
+		string[] parts = playerIp.Split(':');
+		string realIP = parts.Length == 2 ? parts[0] : playerIp;
+
+		using DatabaseReader reader = new DatabaseReader(Path.Combine(ModuleDirectory, "GeoLite2-Country.mmdb"));
+		{
+			try
+			{
+				MaxMind.GeoIP2.Responses.CountryResponse response = reader.Country(realIP);
+				return response.Country.IsoCode ?? "??";
+			}
+			catch (AddressNotFoundException)
+			{
+				Logger.LogError($"The address {realIP} is not in the database.");
+				return "??";
+			}
+			catch (GeoIP2Exception ex)
+			{
+				Logger.LogError($"Error: {ex.Message}");
+				return "??";
+			}
+		}
+	}
+}
